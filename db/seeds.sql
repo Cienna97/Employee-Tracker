df074dc7a4cfe206673fff5f8f0d72d76dbf5ebf
Cienna97 <cmortenson97@gmail.com>
2022-01-27 21:16:07 -0700

    adding in seeds to insert into values

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES
+("Sales"),
+("Management"),
+("Finance & Accounting"),
+("Engineering"),
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+ ('Salesperson', 70000, 1),
+('Sales Lead', 90000, 1),
+('Project Manager', 150000, 2),
+('Operations Manager', 200000, 2),
+('Accountant', 100000, 3),
+('Account Manager', 120000, 3),
+('Software Engineer', 170000, 4),
+('Lead Engineer', 300000, 4);
+
+
+
