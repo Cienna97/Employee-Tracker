5a7d6cdf9c92b108da507fd348d0e3aedfbacdc1
Cienna97 <cmortenson97@gmail.com>
2022-01-28 00:44:09 -0700

    trying to add data into database again unsuccessful

--- server.js
+++ server.js
@@ -121,9 +121,9 @@ showDepartments = () => {
   console.log('Showing all departments...\n');
   const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
 
-  connection.query(sql, (err, rows) => {
+  connection.promise().query(sql, (err, rows) => {
   if (err) throw err;
-  console.table("Response:", rows);
+  console.table(rows);
   promptUser();
   });
 };
