40b25b04cb121182482568f0ca691a62f8df9b9e
Cienna97 <cmortenson97@gmail.com>
2022-01-27 23:07:11 -0700

    show employees and roles for employees

--- server.js
+++ server.js
@@ -110,8 +110,62 @@ const promptUser = () => {
 
     if (choices === "No Action") {
       connection.end()
-      
+
     };
   });
 };
+
+// function that will show all departments available to choose from
+
+showDepartments = () => {
+  console.log('Showing all departments...\n');
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`; 
+
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err;
+    console.table(rows);
+    promptUser();
+  });
+};
+
+// function to show roles available
+
+showRoles = () => {
+  console.log('Showing all roles...\n');
+
+  const sql = `SELECT role.id, role.title, department.name AS department
+               FROM role
+               INNER JOIN department ON role.department_id = department.id`;
+  
+  connection.promise().query(sql, (err, rows) => {
+    if (err) throw err; 
+    console.table(rows); 
+    promptUser();
+  })
+};
+
+showEmployees = () => {
+  console.log('Showing all employees...\n'); 
+  const sql = `SELECT employee.id, 
+                      employee.first_name, 
+                      employee.last_name, 
+                      role.title, 
+                      department.name AS department,
+                      role.salary, 
+                      CONCAT (manager.first_name, " ", manager.last_name) AS manager
+               FROM employee
+                      LEFT JOIN role ON employee.role_id = role.id
+                      LEFT JOIN department ON role.department_id = department.id
+                      LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+
+                      connection.promise().query(sql, (err, rows) => {
+                        if (err) throw err; 
+                        console.table(rows);
+                        promptUser();
+                      });
+                    };
+
+                    
+
 
